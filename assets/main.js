620226f810942a6177124b398b9bdab434d9fd23
Gaston <gastoncapdevila@hotmail.com>
2023-11-16 00:58:27 -0300

    cart menu

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,11 @@ import { categories } from "./data.js";
 // TRAIGO LOS ELEMENOS DEL DOM
 const cardsContainer = document.querySelector(".cards-container");
 const categoriesList = document.querySelector(".categories-list");
+const btnCart = document.querySelector(".btn-cart");
+const cartToggle = document.querySelector(".cart-container");
+const cartMenu = document.querySelector(".cart-products");
+
+let cart = [];
 
 // -----------------------------------------ARRAY DE PRODUCTOS
 // BUSCA CADA OBJETO DEL ARRAY DE PRODUCTOS
@@ -14,24 +19,20 @@ const createCards = (array) => {
 
 // RENDERIZAR LA CARD
 const renderCard = (product) => {
-  const { nombre, precio, marca, imagen } = product;
+  const { id, nombre, precio, marca, imagen } = product;
   cardsContainer.innerHTML += `
-        <div class="card">
+           <div class="card">
             <img
-              src=${imagen}
+             src=${imagen}
               alt="foto-producto"
               class="img-card"
             />
             <div class="card-info-container">
               <h2 class="card-title">${nombre}</h2>
               <p>${marca}</p>
-              <div class="card-button">
-                <h3>"$ ${precio}"</h3>
-                <div class="btn-card">
-                  <a href=".cart.html">Agregar</a>
-                </div>
+              <h3>"$ ${precio}"</h3>
               </div>
-            </div>
+              <button class="btn-add-cart" data-id=${id}>Agregar</button>
           </div>`;
 };
 
@@ -65,13 +66,74 @@ const handleCategory = (e) => {
   createCards(categorySelected);
 };
 
-//---------------------------------------------- INICIAR
+// -----------------------------------------TOGGLE CART---
+const togleCart = () => {
+  if (cartToggle.classList.contains("togle-close-cart")) {
+    cartToggle.classList.remove("togle-close-cart");
+  } else {
+    cartToggle.classList.add("togle-close-cart");
+  }
+};
+
+const addToCart = (e) => {
+  const idProduct = Number(e.target.dataset.id);
+  const cardSelected = () => {
+    return products.find((product) => product.id === idProduct);
+  };
+  const cartProduct = cardSelected();
+  cart.push(cartProduct);
+  cartMenu.innerHTML = "";
+  createPorductsCart(cart);
+};
+
+const createPorductsCart = (cart) => {
+  cart.map(renderCartProduct);
+};
+
+const renderCartProduct = (product) => {
+  console.log(cart);
+  const { nombre, precio, marca, imagen } = product;
+  cartMenu.innerHTML += `
+             <div class="cart-product">
+             <div class="cart-product-content">
+                <div class="img-cart-container">
+                  <img
+                  src=${imagen}
+                    class="cart-img"
+                    />
+                    </div>
+                    <div class="info-cart-container">
+                  <h3>${nombre}</h3>
+                  <span>${marca}</span>
+                  <p>${precio}</p>
+                  </div>
+              </div>
+              <div class="total-product-container">
+                <div class="plus-minus-container">
+                <span>-</span>
+                  <span>5</span>
+                  <span>+</span>
+                </div>
+                <p>subtotal</p>
+                </div>
+                <div class="product-delete">
+                <button>X</button>
+                </div>
+                </div>
+                `;
+};
+//---------------------------------------------- INICIAR --------------
 const init = () => {
   createCategories();
   createCards(products);
-  const htmlToArray = [...categoriesList.children];
-  htmlToArray.map((btn) => btn.addEventListener("click", handleCategory));
+  const categoriesListArray = [...categoriesList.children];
+  categoriesListArray.map((btn) =>
+    btn.addEventListener("click", handleCategory)
+  );
+  btnCart.addEventListener("click", togleCart);
+  const cardsArray = [...cardsContainer.children];
+  cardsArray.map((card) =>
+    card.childNodes[5].addEventListener("click", addToCart)
+  );
 };
 init();
-
-// console.log(htmlToArray);
